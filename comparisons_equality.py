930ea5439f111d61350ec9698eee54f51fd979c8
lisboalien <lisboalien@gmail.com>
2018-02-11 21:56:04 -0200

    Some comparisons tests

---
+++ comparisons_equality.py
@@ -0,0 +1,22 @@
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5 * 15 != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with an integer
+print(True == 1)
+
+# Comparison of integers
+x = -3 * 6
+print(x>=-10)
+
+# Comparison of strings
+y = "test"
+print("test"<=y)
+
+# Comparison of booleans
+print(True>False)
