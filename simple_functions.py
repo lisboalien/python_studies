90743af95c008ae3e9bf7d7c85380020bc6f63b1
lisboalien <lisboalien@gmail.com>
2018-02-12 17:05:59 -0200

    Simple functions example

---
+++ simple_functions.py
@@ -0,0 +1,46 @@
+# Define the function shout
+def shout_1():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout_1()
+
+############################################################
+# Single Parameter Function
+############################################################
+
+# Define shout with the parameter, word
+def shout_2(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout_2('congratulations')
+
+###########################################################
+# Functions with returns
+###########################################################
+
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + "!!!"
+
+    # Replace print with return
+    return shout_word
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)
