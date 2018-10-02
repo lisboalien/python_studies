b9fd4db997045d578c323b136df1faa8926b24a4
Aline de Farias Lisboa <lisboalien@gmail.com>
2018-10-02 10:03:41 -0300

    New study files

--- dictionary_europe.py
+++ dictionary_europe.py
@@ -7,3 +7,35 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo
 
 # Print europe
 print(europe)
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
+# Add italy to europe
+europe['italy'] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+# Messing Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del europe['australia']
+
+# Print europe
+print(europe)
